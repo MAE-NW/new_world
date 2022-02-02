743af1537c6d56934616ce3981b97dd58efd28f6
Mohammid06 <95246773+Mohammid06@users.noreply.github.com>
2022-02-02 10:52:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-copyright© 2022
-final Button button = new Button(ProjectinActivity.this); button.setText("Your Button"); button.setLayoutParams(new LinearLayout.LayoutParams(android.widget.LinearLayout.LayoutParams.WRAP_CONTENT, android.widget.LinearLayout.LayoutParams.WRAP_CONTENT)); linear.addView(mybutton)
+{
+"ok": "hiiii"
+}
