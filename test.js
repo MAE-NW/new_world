9de79096608a56e736b0ccfa384051334231f684
Mohammid06 <95246773+Mohammid06@users.noreply.github.com>
2022-03-10 21:16:09 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-on_response = new Gson().fromJson(_response, new TypeToken<HashMap<String, Object>>(){}.getType());
-if (on_response.containsKey("link")) {
-	textview2.setText(on_response.get("link").toString());
-	SketchwareUtil.showMessage(getApplicationContext(), "Successful Shorten Link");
-}
-else {
-	SketchwareUtil.showMessage(getApplicationContext(), "Error ");
-}
+momadooo hh
